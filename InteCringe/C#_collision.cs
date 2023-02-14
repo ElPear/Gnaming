ad4539211239c10f7d62d4aa554572d56b7306c9
Päron <105945198+ElPear@users.noreply.github.com>
2023-02-14 11:04:28 +0100

    Update C#_collision.cs

--- InteCringe/C#_collision.cs
+++ InteCringe/C#_collision.cs
@@ -1,11 +1,12 @@
         private bool Colliding(PictureBox item_one, PictureBox item_two)
         {
-            if (item_one.Top <= item_two.Bottom && item_one.Top >= item_two.Top || item_one.Bottom <= item_two.Bottom && item_one.Bottom >= item_two.Top)
-            {
-                if (item_one.Left >= item_two.Left && item_one.Left <= item_two.Right || item_one.Right <= item_two.Right && item_one.Right >= item_two.Left)
-                {
-                    return true;
-                }
-            }
-            return false;
+            if (p1.Location.X + p1.Width < p2.Location.X) 
+                return false;
+            if (p2.Location.X + p2.Width < p1.Location.X)
+                return false;
+            if (p1.Location.Y + p1.Height < p2.Location.Y)
+                return false;
+            if (p2.Location.Y + p2.Height < p1.Location.Y)
+                return false;
+            return true;
         }
